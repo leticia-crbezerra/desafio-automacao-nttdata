2de961c91b35f4383e033062e2026d237efbb24d
Letícia Bezerra <leticia.cribeiro317@gmail.com>
2024-09-27 17:33:52 -0300

    feat: Realizando login via API

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      baseUrl: "https://front.serverest.dev/login",
-    },
+    //baseUrl: 'https://serverest.dev',
+  },
+  env: {
+    login_email: 'teste@teste.com',
+    login_password: '12345'
+  }
+
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -9,8 +9,6 @@
 // ***********************************************
 //
 //
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
 //
 //
 // -- This is a child command --
@@ -23,3 +21,13 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('loginViaAPI', (email, password) => {
+    cy.request('POST', 'https://serverest.dev/login', {
+        email: email,
+        password: password,
+    }).then((response) => {
+        window.localStorage.setItem('serverest/userToken', response.body.authorization);
+    })
+})
